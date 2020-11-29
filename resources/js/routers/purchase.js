c5cd726d4a8e8071d18a13a9d526ec73748ce5f3
BuzzerByte <rujyi94@hotmail.com>
2020-11-30 06:40:41 +0800

    updating ui routers

---
+++ resources/js/routers/purchase.js
@@ -0,0 +1,29 @@
+import * as Purchases from '../views/__backoffice/purchases';
+
+const resources = [
+    {
+        name: 'purchases.index',
+        path: '/purchases',
+        component: Purchases.List,
+    },
+
+    {
+        name: 'purchases.create',
+        path: '/purchases/create',
+        component: Purchases.Create,
+    }
+].map(route => {
+    route.name = `resources.${route.name}`;
+    route.path = `/resources${route.path}`;
+
+    return route;
+});
+
+export default [
+    ...resources,
+].map(route => {
+    route.name = `purchases.${route.name}`;
+    route.auth = true;
+
+    return route;
+});
